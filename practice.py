3f269c1c0ca6954ac0fb55bb17d8e837fa93f8fb
NaviCo-de <aridarrell30@gmail.com>
2026-02-09 14:46:54 +0700

    Push Power

--- practice.py
+++ practice.py
@@ -1,7 +1,28 @@
 class Calculator:
-    def divide(self, a, b):
-        return a/b
-    
+    def power(self, a, b):
+        return a + b
+
+    def power(self, a, b):
+        return a - b
+
+    def power(self, a, b):
+        return a * b
+
+    def power(self, a, b):
+        return a / b
+
+    def power(self, a, b):
+        return a % b
+
+    def power(self, a, b):
+        return a ** b
+
+
 if __name__ == "__main__":
     calc = Calculator()
-    print("Division: ", calc.divide(10,5))
+    print("Addition:", calc.power(10, 5))
+    print("Subtraction:", calc.power(10, 5))
+    print("Multiplication:", calc.power(10, 5))
+    print("Division:", calc.power(10, 5))
+    print("Modulo:", calc.power(10, 5))
+    print("Power:", calc.power(10, 5))
