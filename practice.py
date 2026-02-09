29f82de644f1b5217aff9a6b89e83b737c4a9f18
rafasoelistiono <rafasoelistiono@gmail.com>
2026-02-09 14:50:35 +0700

    update divide

--- practice.py
+++ practice.py
@@ -1,7 +1,28 @@
 class Calculator:
     def divide(self, a, b):
-        return a/b
-    
+        return a + b
+
+    def divide(self, a, b):
+        return a - b
+
+    def divide(self, a, b):
+        return a * b
+
+    def divide(self, a, b):
+        return a / b
+
+    def divide(self, a, b):
+        return a % b
+
+    def divide(self, a, b):
+        return a ** b
+
+
 if __name__ == "__main__":
     calc = Calculator()
-    print("Division: ", calc.divide(10,5))
+    print("Addition:", calc.divide(10, 5))
+    print("Subtraction:", calc.divide(10, 5))
+    print("Multiplication:", calc.divided(10, 5))
+    print("Division:", calc.divide(10, 5))
+    print("Modulo:", calc.divide(10, 5))
+    print("Power:", calc.divide(10, 5))
