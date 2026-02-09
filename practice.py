8d6fa5769715d025ac0320e4af370eef4d7fdaca
jonathanrundjan <jonathan.yitskhaq@ui.ac.id>
2026-02-09 14:44:51 +0700

    subtract ishak

--- practice.py
+++ practice.py
@@ -20,9 +20,9 @@ class Calculator:
 
 if __name__ == "__main__":
     calc = Calculator()
-    print("Addition:", calc.add(10, 5))
+    print("Addition:", calc.subtract(10, 5))
     print("Subtraction:", calc.subtract(10, 5))
-    print("Multiplication:", calc.multiply(10, 5))
-    print("Division:", calc.divide(10, 5))
-    print("Modulo:", calc.modulo(10, 5))
-    print("Power:", calc.power(10, 5))
+    print("Multiplication:", calc.subtract(10, 5))
+    print("Division:", calc.subtract(10, 5))
+    print("Modulo:", calc.subtract(10, 5))
+    print("Power:", calc.subtract(10, 5))
