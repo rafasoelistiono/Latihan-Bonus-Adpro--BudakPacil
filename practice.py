f2a5c631d80e61afd801171e7f94c03ea63d05b2
jonathanrundjan <jonathan.yitskhaq@ui.ac.id>
2026-02-09 14:43:56 +0700

    done substract ishak

---
+++ practice.py
@@ -0,0 +1,28 @@
+class Calculator:
+    def subtract(self, a, b):
+        return a + b
+
+    def subtract(self, a, b):
+        return a - b
+
+    def subtract(self, a, b):
+        return a * b
+
+    def subtract(self, a, b):
+        return a / b
+
+    def subtract(self, a, b):
+        return a % b
+
+    def subtract(self, a, b):
+        return a ** b
+
+
+if __name__ == "__main__":
+    calc = Calculator()
+    print("Addition:", calc.add(10, 5))
+    print("Subtraction:", calc.subtract(10, 5))
+    print("Multiplication:", calc.multiply(10, 5))
+    print("Division:", calc.divide(10, 5))
+    print("Modulo:", calc.modulo(10, 5))
+    print("Power:", calc.power(10, 5))
